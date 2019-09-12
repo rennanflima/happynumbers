f09c845ab25de2808a772f2b1aee10eef1696d1b
Rennan Francisco Messias de Lima <mrennan.lima@gmail.com>
2019-09-12 11:42:41 -0500

    Add happy numbers

---
+++ happynumbers.py
@@ -0,0 +1,17 @@
+def sum_of_squares(number):
+    string = str(number)
+    digits = [int(char) ** 2 for char in string]
+    return sum(digits)
+
+def happy(number):
+    box = []
+    n = number
+    while n != 1 and n not in box:
+        box.append(n)
+        n = sum_of_squares(n)
+    return n == 1
+
+
+assert sum_of_squares(130) == 10
+assert all(happy(n) for n in (1, 10, 100, 97, 130))
+assert not happy(4)
